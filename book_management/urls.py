e87bfa07b7600cd8eb77068d97a9afc2f191a74a
Victor-Krupeichenko <71899727+Victor-Krupeichenko@users.noreply.github.com>
2023-08-20 15:45:07 +0300

    Добавлен маршрут для контроллера обновления информации о книге

--- book_management/urls.py
+++ book_management/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import CreateBook, AllBookView, DetailBookView
+from .views import CreateBook, AllBookView, DetailBookView, UpdateBook
 
 urlpatterns = [
     path('', AllBookView.as_view(), name="home"),
     path('add-book/', CreateBook.as_view(), name="add_book"),
     path("detail-book-view/<str:slug>/", DetailBookView.as_view(), name="detail_book"),
+    path("update-info-for-book/<str:slug>/", UpdateBook.as_view(), name="update_book"),
 ]
