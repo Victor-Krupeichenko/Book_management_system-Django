522ca27d60119238978afbbf94b16d07a099ba0e
Victor-Krupeichenko <71899727+Victor-Krupeichenko@users.noreply.github.com>
2023-08-20 14:28:45 +0300

    Добавлен маршрут для контроллера показа всех книг

--- book_management/urls.py
+++ book_management/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import CreateBook
+from .views import CreateBook, AllBookView
 
 urlpatterns = [
+    path('', AllBookView.as_view(), name="home"),
     path('add-book/', CreateBook.as_view(), name="add_book"),
 ]
