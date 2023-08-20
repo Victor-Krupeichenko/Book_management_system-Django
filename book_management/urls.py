db8cc4227dd1910f718f6b0510baebbe6419fba9
Victor-Krupeichenko <71899727+Victor-Krupeichenko@users.noreply.github.com>
2023-08-20 14:42:58 +0300

    Добавлен маршрут для контролера детального просмотра книги

--- book_management/urls.py
+++ book_management/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CreateBook, AllBookView
+from .views import CreateBook, AllBookView, DetailBookView
 
 urlpatterns = [
     path('', AllBookView.as_view(), name="home"),
     path('add-book/', CreateBook.as_view(), name="add_book"),
+    path("detail-book-view/<str:slug>/", DetailBookView.as_view(), name="detail_book"),
 ]
