b2b6ffcaed857c4f8fb903fe463942f467232202
Victor-Krupeichenko <71899727+Victor-Krupeichenko@users.noreply.github.com>
2023-08-21 11:15:02 +0300

    Добавлены маршруты для контроллеров получения всех списка всех авторов и получения списка книг конкретного автора

--- book_management/urls.py
+++ book_management/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import CreateBook, AllBookView, DetailBookView, UpdateBook, DeleteBook
+from .views import CreateBook, AllBookView, DetailBookView, UpdateBook, DeleteBook, AllAuthorView, AllAuthrBook
 
 urlpatterns = [
-    path('', AllBookView.as_view(), name="home"),
-    path('add-book/', CreateBook.as_view(), name="add_book"),
+    path("", AllBookView.as_view(), name="home"),
+    path("add-book/", CreateBook.as_view(), name="add_book"),
     path("detail-book-view/<str:slug>/", DetailBookView.as_view(), name="detail_book"),
     path("update-info-for-book/<str:slug>/", UpdateBook.as_view(), name="update_book"),
     path("delete-book/<str:slug>/", DeleteBook.as_view(), name="delete_book"),
+    path("all-author/", AllAuthorView.as_view(), name="all_author"),
+    path("all-author-book/<str:slug>/", AllAuthrBook.as_view(), name="all_author_book"),
 ]
