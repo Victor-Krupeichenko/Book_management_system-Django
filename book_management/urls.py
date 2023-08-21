1b458cd2beae016e5f55096f7bc787de48cfee36
Victor-Krupeichenko <71899727+Victor-Krupeichenko@users.noreply.github.com>
2023-08-21 10:25:25 +0300

    Добавлен маршрут для контроллера удаления книги

--- book_management/urls.py
+++ book_management/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import CreateBook, AllBookView, DetailBookView, UpdateBook
+from .views import CreateBook, AllBookView, DetailBookView, UpdateBook, DeleteBook
 
 urlpatterns = [
     path('', AllBookView.as_view(), name="home"),
     path('add-book/', CreateBook.as_view(), name="add_book"),
     path("detail-book-view/<str:slug>/", DetailBookView.as_view(), name="detail_book"),
     path("update-info-for-book/<str:slug>/", UpdateBook.as_view(), name="update_book"),
+    path("delete-book/<str:slug>/", DeleteBook.as_view(), name="delete_book"),
 ]
