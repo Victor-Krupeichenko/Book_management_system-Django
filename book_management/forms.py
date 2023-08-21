e7af62349681604fab515082d47b65a11e83f87c
Victor-Krupeichenko <71899727+Victor-Krupeichenko@users.noreply.github.com>
2023-08-21 12:27:25 +0300

    Добавление отдельного класса виджетов для полей, добавлена форма для модели publisher

--- book_management/forms.py
+++ book_management/forms.py
@@ -1,9 +1,21 @@
 from django import forms
 
-from .models import Book
+from .models import Book, Publisher, Author, Language, Genre
 
 
-class FormBook(forms.ModelForm):
+class BaseFieldForm(forms.ModelForm):
+    """Добавляет виджеты для полей формы"""
+
+    def __init__(self, *args, **kwargs):
+        """Переопределение метода и добавление виджетов в форму"""
+        super().__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs["class"] = "form-control"
+            self.fields[field].widget.attrs["placeholder"] = self.fields[field].label
+            self.fields[field].label = ""
+
+
+class FormBook(BaseFieldForm):
     """Форма для добавления книги"""
 
     class Meta:
@@ -13,8 +25,12 @@ class FormBook(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         """Переопределение метода и добавление виджетов в форму"""
         super().__init__(*args, **kwargs)
-        for field in self.fields:
-            self.fields[field].widget.attrs["class"] = "form-control"
-            self.fields[field].widget.attrs["placeholder"] = self.fields[field].label
-            self.fields[field].label = ""
         self.fields["publisher"].empty_label = "Выбери издателя"
+
+
+class FormPublisher(BaseFieldForm):
+    """Форма для модели Publisher"""
+
+    class Meta:
+        model = Publisher
+        fields = ["title", "address", "email_address"]
