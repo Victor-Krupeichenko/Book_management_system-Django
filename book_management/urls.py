a86fd08b0d04a756999a59564c9411085861ad6a
Victor-Krupeichenko <71899727+Victor-Krupeichenko@users.noreply.github.com>
2023-08-21 17:02:44 +0300

    Добавлены маршруты для контроллеров модели author и модели publisher

--- book_management/urls.py
+++ book_management/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-from .views import CreateBook, AllBookView, DetailBookView, UpdateBook, DeleteBook, AllAuthorView, AllAuthrBook
+from .views import (
+    CreateBook, AllBookView, DetailBookView, UpdateBook, DeleteBook, AllAuthorView, AllAuthrBook, CreatePublisher,
+    AllPublisherView, AllPublisherBook, CreateAuthor
+)
 
 urlpatterns = [
     path("", AllBookView.as_view(), name="home"),
@@ -9,4 +12,8 @@ urlpatterns = [
     path("delete-book/<str:slug>/", DeleteBook.as_view(), name="delete_book"),
     path("all-author/", AllAuthorView.as_view(), name="all_author"),
     path("all-author-book/<str:slug>/", AllAuthrBook.as_view(), name="all_author_book"),
+    path("create_publisher/", CreatePublisher.as_view(), name="create_publisher"),
+    path("all-publisher/", AllPublisherView.as_view(), name="all_publisher"),
+    path("all-publisher-book/<str:slug>/", AllPublisherBook.as_view(), name="all_publisher_book"),
+    path("add-author/", CreateAuthor.as_view(), name="add_author"),
 ]
