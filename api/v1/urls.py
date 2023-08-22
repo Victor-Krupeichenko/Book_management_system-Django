645447bc1caccc9f8f9a344ab5409f5f5d77f630
Victor-Krupeichenko <71899727+Victor-Krupeichenko@users.noreply.github.com>
2023-08-22 14:46:11 +0300

    Добавлены маршруты для представлений: - Показать все жанры; - Добавить жанр; - Удалить жанр; - Обновить жанр.

--- api/v1/urls.py
+++ api/v1/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from api.v1.views import (
-    ListLanguage, CreateLanguage, DeleteLanguage, UpdateLanguage
+    ListLanguage, CreateLanguage, DeleteLanguage, UpdateLanguage, ListGenre, CreateGenre, DeleteGenre, UpdateGenre
 )
 
 urlpatterns = [
@@ -8,4 +8,8 @@ urlpatterns = [
     path("add-language/", CreateLanguage.as_view()),
     path("delete-language/<int:pk>/", DeleteLanguage.as_view()),
     path("update-language/<int:pk>/", UpdateLanguage.as_view()),
+    path("list-genres/", ListGenre.as_view()),
+    path("add-genre/", CreateGenre.as_view()),
+    path("delete-genre/<int:pk>/", DeleteGenre.as_view()),
+    path("update-genre/<int:pk>/", UpdateGenre.as_view()),
 ]
