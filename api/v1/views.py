8d2974a614febdc948dc4be26279fb61c07fe82b
Victor-Krupeichenko <71899727+Victor-Krupeichenko@users.noreply.github.com>
2023-08-22 14:44:48 +0300

    Добавлены представление для: - Показать все жанры; - Добавить жанр; - Удалить жанр; - Обновить жанр.

--- api/v1/views.py
+++ api/v1/views.py
@@ -1,8 +1,8 @@
 from rest_framework.response import Response
 from rest_framework.generics import ListAPIView, CreateAPIView, DestroyAPIView, UpdateAPIView
 from rest_framework import status
-from api.v1.serializers import LanguageSerializer
-from book_management.models import Language
+from api.v1.serializers import LanguageSerializer, GenreSerializer
+from book_management.models import Language, Genre
 from django.http import Http404
 
 
@@ -80,5 +80,81 @@ class UpdateLanguage(UpdateAPIView):
                 "error": "Ни один язык не соответствует заданному запросу."
             }
             return Response(data=response, status=status.HTTP_404_NOT_FOUND)
+
+
+class ListGenre(ListAPIView):
+    """Показ всех жанров"""
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+
+class CreateGenre(CreateAPIView):
+    """Создание жанра"""
+    serializer_class = GenreSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            self.perform_create(serializer)
+            response = {
+                "message": f"Жанр {serializer.data.get('title')} добавлен"
+            }
+            return Response(data=response, status=status.HTTP_201_CREATED)
+        else:
+            response = {
+                "error": serializer.errors
+            }
+            return Response(data=response, status=status.HTTP_400_BAD_REQUEST)
+
+
+class DeleteGenre(DestroyAPIView):
+    """Удаление жанра"""
+    serializer_class = GenreSerializer
+    queryset = Genre.objects.all()
+
+    def destroy(self, request, *args, **kwargs):
+        try:
+            instance = self.get_object()
+            self.perform_destroy(instance)
+            response = {
+                "message": f"Жанр {instance.title} удален."
+            }
+            return Response(data=response, status=status.HTTP_204_NO_CONTENT)
+        except Http404:
+            response = {
+                "error": "Ни один из жанров не соответствует запросу."
+            }
+            return Response(data=response, status=status.HTTP_404_NOT_FOUND)
+
+
+class UpdateGenre(UpdateAPIView):
+    """Обновление жанра"""
+    serializer_class = GenreSerializer
+    queryset = Genre.objects.all()
+
+    def update(self, request, *args, **kwargs):
+        try:
+            instance = self.get_object()
+            serializer = self.get_serializer(instance, request.data)
+            old_genre = instance.title
+            if serializer.is_valid():
+                self.perform_update(serializer)
+                response = {
+                    "message": f"Жанр {old_genre} обновлен на {instance.title}"
+                }
+                return Response(data=response, status=status.HTTP_200_OK)
+            return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Http404:
+            response = {
+                "error": "Ни один из жанров не соответствует запросу."
+            }
+            return Response(data=response, status=status.HTTP_404_NOT_FOUND)
+
+
 
 
